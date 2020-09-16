1a45103fcdd565398557690a17b81e6f2a4e238f
Peter Lightbody <pet1330@gmail.com>
2020-09-16 19:24:44 +0100

    Allow deferred error handling

--- flask_sieve/__init__.py
+++ flask_sieve/__init__.py
@@ -2,6 +2,11 @@ from .requests import JsonRequest, FormRequest
 from .validator import validate, Validator
 from .exceptions import ValidationException, register_error_handler
 
+
 class Sieve:
-    def __init__(self, app):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
         register_error_handler(app)
