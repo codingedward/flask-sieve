3f085246eedce56a9eda851b5d0998af2d9c150e
Peter Lightbody <pet1330@gmail.com>
2020-09-17 14:00:23 +0100

    Adds and tests default configuration options

--- flask_sieve/exceptions.py
+++ flask_sieve/exceptions.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 
+
 class ValidationException(Exception):
     def __init__(self, errors):
         self.errors = errors
@@ -7,13 +8,19 @@ class ValidationException(Exception):
 
 def register_error_handler(app):
     def validations_error_handler(ex):
-        return jsonify({
-            'success': False,
-            'message': 'Validation error',
+        response = {
+            'message': app.config.get('SIEVE_DEFAULT_RESPONSE_MESSAGE', 'Validation error'),
             'errors': ex.errors
-        }), app.config.get('SIEVE_INVALID_STATUS_CODE', 400)
+        }
+
+        if app.config.get('SIEVE_INCLUDE_SUCCESS_KEY'):
+            response['success'] = False
+
+        if app.config.get('SIEVE_RESPONSE_WRAPPER'):
+            response = {app.config.get('SIEVE_RESPONSE_WRAPPER'): response}
+
+        return jsonify(response), app.config.get('SIEVE_INVALID_STATUS_CODE', 400)
     app.register_error_handler(
         ValidationException,
         validations_error_handler
     )
-
